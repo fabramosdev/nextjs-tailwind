766ac6818c60aceaf2bbc6bd94126959b9487b0d
Fabiano Ramos <fabiano.suporteinfo@gmail.com>
2020-12-27 21:45:26 -0300

    first commit

---
+++ pages/index.tsx
@@ -0,0 +1,13 @@
+
+
+const App: React.FC = () => {
+  return(
+    <div className="flex-column justify-center">
+      <p className="font-sans text-xl">Esse é o nosso jeito de inovar</p>
+      <p className="">O Box UP é um processo para evidenciar o valor que geramos para o cliente e, assim, investir naqueles produtos que serão bem aceitos no mercado.</p>
+    </div>
+    
+  )
+}
+
+export default App;
